fc51b36701845d528c2b637351dd308a2db1a7be
Dayasagar <dayasagar301@gmail.com>
2024-08-05 12:25:11 +0530

    assignment   updated

--- Backend/olx-clone/server.js
+++ Backend/olx-clone/server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const connectDB = require('./config/db'); // Import the database connection
-const authRoutes = require('./routes/authRoutes');
-const itemRoutes = require('./routes/itemRoutes');
-const cors = require('cors');
-const path = require('path'); // Import path module
+const express = require("express");
+const connectDB = require("./config/db"); // Import the database connection
+const authRoutes = require("./routes/authRoutes");
+const itemRoutes = require("./routes/itemRoutes");
+const cors = require("cors");
+const path = require("path"); // Import path module
 const app = express();
 
 // Use CORS
-app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+  })
+);
 
 // Connect to MongoDB
 connectDB();
@@ -18,16 +20,16 @@ connectDB();
 app.use(express.json());
 
 // Serve static files from the 'uploads' directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/items', itemRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 // Start the server
